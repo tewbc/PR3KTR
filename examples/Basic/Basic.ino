cf3cfd8d51979cd2c88dad836aa837ba0b086ab5
Varong Boonchirdchoo <54541042+tewbc@users.noreply.github.com>
2022-08-17 22:33:58 +0700

    Update Basic.ino

--- examples/Basic/Basic.ino
+++ examples/Basic/Basic.ino
@@ -3,7 +3,7 @@ PR3KTR npk(Serial2);                             // decare class to npk name.
 PR3KTR::DATA data;                               // decare DATA structure to data
 
 void setup() {
-  Serial.begin(9600);                            // GPIO1,     GPIO3 (TX/RX pin on ESP-32 Development Board)
+  Serial.begin(9600);                            // GPIO1,   GPIO3 (TX/RX pin on ESP-32 Development Board)
   Serial2.begin(4800);                           // GPIO17, GPIO16 (TX/RX pin on ESP-32 Development Board)
   npk.setID(0x01);                               // Set Slave ID
 }
