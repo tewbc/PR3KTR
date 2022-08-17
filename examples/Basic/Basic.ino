857dfb739d0343e0ec7df974ec21349562b7d66f
Varong Boonchirdchoo <54541042+tewbc@users.noreply.github.com>
2022-08-17 22:32:58 +0700

    Add files via upload

--- examples/Basic/Basic.ino
+++ examples/Basic/Basic.ino
@@ -1,27 +1,28 @@
 #include "PR3KTR.h"
-PR3KTR npk(Serial2);		// decare class to other name.
-PR3KTR::DATA data;		  // decare DATA structure to other name
+PR3KTR npk(Serial2);                        // decare class to npk name.
+PR3KTR::DATA data;                         // decare DATA structure to data
 
 void setup() {
-  Serial.begin(9600);		// GPIO1, GPIO3 (TX/RX pin on ESP-12E Development Board)
-  Serial2.begin(4800);	// GPIO15, GPIO13 (TX/RX pin on ESP-12E Development Board)
-  npk.setID(0x01);		  // Set Slave ID
+  Serial.begin(9600);                            // GPIO1,     GPIO3 (TX/RX pin on ESP-32 Development Board)
+  Serial2.begin(4800);                          // GPIO17, GPIO16 (TX/RX pin on ESP-32 Development Board)
+  npk.setID(0x01);                                // Set Slave ID
 }
 
 void loop() {
-  if (npk.getNPK(data))	// Get NPK at once
-  {
+  if (npk.getN(data))   {                         //Get N value
     Serial.print("N (mg/L): ");
     Serial.println(data.NITROGEN);
-
+  }
+  delay(200);
+  if (npk.getP(data))  {                          //Get P value
     Serial.print("P (mg/L): ");
     Serial.println(data.POTASSIUM);
-
+  }
+  delay(200);
+  if (npk.getK(data))   {                         //Get K value
     Serial.print("K (mg/L): ");
     Serial.println(data.PHOSPHORUS);
-
-    Serial1.println();
   }
-  delay(5000);
+  delay(200);
   // Do other stuff...
 }
